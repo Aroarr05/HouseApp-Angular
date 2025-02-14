f4e9fe51c30c3e06134ac1b26cb5f7db33b0a33e
Aroa <aroa.rivas.2003@gmail.com>
2025-02-14 18:54:17 +0100

    almacenado en LocalStorage

--- src/app/components/loging/loging.component.ts
+++ src/app/components/loging/loging.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'; 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -13,7 +13,7 @@ export class LogingComponent {
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -21,11 +21,23 @@ export class LogingComponent {
     })
   }
 
-  onSubmit(){
-    if(this.loginForm.valid){
-      console.log("Form submitted:", this.loginForm.value);
-    }else{
-      console.log("Form is invalid");
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
+      // Guardar los datos del formulario en el localStorage
+      localStorage.setItem('loginData', JSON.stringify(this.loginForm.value));
+
+      alert('Form data has been saved!');
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  ngOnInit() {
+    // Recuperar datos del localStorage si están disponibles
+    const savedData = localStorage.getItem('loginData');
+    if (savedData) {
+      this.loginForm.patchValue(JSON.parse(savedData));  // Rellenar los campos con los datos guardados
     }
   }
 
