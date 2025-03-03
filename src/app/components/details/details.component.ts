98f233e31274d445066ed852bdb3cb7275446fbd
Aroa <aroa.rivas.2003@gmail.com>
2025-03-03 21:21:00 +0100

    funciona bien

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -13,11 +13,11 @@ import { Coordinates } from '../../model/housinglocation';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-
 export class DetailsComponent implements OnInit, AfterViewInit {
   housingLocation: HousingLocation | undefined;
   private mapa: any;
   private L: any;
+  private circleLayer: any; // Variable para el círculo morado
 
   applyForm = new FormGroup({
     firstName: new FormControl(''),
@@ -31,11 +31,13 @@ export class DetailsComponent implements OnInit, AfterViewInit {
     @Inject(PLATFORM_ID) private platformId: any
   ) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
     const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
-    this.housingService.getHousingLocationById(housingLocationId).then((data) => {
-      this.housingLocation = data;
-    });
+    this.housingLocation = await this.housingService.getHousingLocationById(housingLocationId);
+
+    if (isPlatformBrowser(this.platformId) && this.L && this.housingLocation?.coordinates) {
+      this.verMapa(this.housingLocation.coordinates);
+    }
   }
 
   submitApplication(): void {
@@ -57,29 +59,19 @@ export class DetailsComponent implements OnInit, AfterViewInit {
     }
   }
 
+  
   private inicializarMapa(): void {
-    if (!this.mapa) {
-      this.mapa = this.L.map('map').setView([0, 0], 2);
-      
-      this.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(this.mapa);
-
-      this.L.circle([0, 0], {
-        color: 'purple',
-        fillColor: 'purple',
-        fillOpacity: 0.6,
-        radius: 50000
-      }).addTo(this.mapa)
-        .bindPopup('<b>Ubicación inicial</b>')
-        .openPopup();
-    }
-
+    if (!this.L || this.mapa) return;
+    this.mapa = this.L.map('map').setView([0, 0], 2);
+    this.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.mapa);
+    
     if (this.housingLocation?.coordinates) {
-      this.verMapa(this.housingLocation.coordinates);
+      setTimeout(() => this.verMapa(this.housingLocation!.coordinates), 500);
     }
   }
-
+  
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       import('leaflet').then((L: typeof import('leaflet')) => {
@@ -88,18 +80,27 @@ export class DetailsComponent implements OnInit, AfterViewInit {
       });
     }
   }
-
+  
   verMapa(coordenadas: Coordinates): void {
     if (isPlatformBrowser(this.platformId) && this.mapa) {
       this.mapa.setView([coordenadas.latitude, coordenadas.longitude], 13);
-      this.L.circle([coordenadas.latitude, coordenadas.longitude], {
+      this.mapa.eachLayer((layer: any) => {
+        if (layer instanceof this.L.Marker || layer === this.circleLayer) {
+          this.mapa.removeLayer(layer);
+        }
+      });
+
+      this.L.marker([coordenadas.latitude, coordenadas.longitude])
+        .addTo(this.mapa)
+        .bindPopup(`<b>Ubicación: ${coordenadas.latitude}, ${coordenadas.longitude}</b>`)
+        .openPopup();
+
+      this.circleLayer = this.L.circle([coordenadas.latitude, coordenadas.longitude], {
         color: 'purple',
         fillColor: 'purple',
         fillOpacity: 0.6,
         radius: 500
-      }).addTo(this.mapa)
-        .bindPopup(`<b>${coordenadas.latitude}, ${coordenadas.longitude}</b>`)
-        .openPopup();
+      }).addTo(this.mapa);
     }
   }
 }
